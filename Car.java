7c9058e6504ff3cb3d29cce29e178cac6f8761cc
Am4deuss <isak.treptow@hotmail.se>
2025-01-27 19:31:25 +0100

    Uppgift 1 + Start av Uppgift 2

---
+++ Car.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+
+public class Car implements Movable {
+
+    protected int nrDoors; // Number of doors on the car
+    protected double enginePower; // Engine power of the car
+    protected double currentSpeed; // The current speed of the car
+    protected Color color; // Color of the car
+    protected String modelName; // The car model name
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    @Override
+    public void move() {
+        
+    }
+
+    @Override
+    public void turnLeft() {
+
+    }
+
+    @Override
+    public void turnRight() {
+
+    }
+}
